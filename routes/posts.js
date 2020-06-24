18ade3652fd6d524e525779c392ea1f1c4a03e3f
Jorge <jorgediazok@gmail.com>
2020-06-24 02:09:58 -0300

    first link with database

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,29 @@
 const express = require('express');
 
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
-  res.send('LALALOVEYOU');
+  res.send('we are on posts');
 });
 
 router.get('/specific', (req, res) => {
-  res.send('specific post');
+  res.send('we are on specific posts');
+});
+
+router.post('/', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+  post
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
 });
 
 module.exports = router;
